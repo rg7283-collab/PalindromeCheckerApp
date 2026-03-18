477ff3aa364ee071721469271138ad80d7f8fd28
rg7283-collab <rg7283@srmist.edu.in>
2026-03-18 09:29:01 +0530

    UC1: Application entry and welcome message

---
+++ src/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,12 @@
+public class UseCase1PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("==============================");
+        System.out.println("PALINDROME CHECKER APP");
+        System.out.println("Version : 1.0");
+        System.out.println("==============================");
+
+        System.out.println("Welcome to the Palindrome Checker Application");
+    }
+}
