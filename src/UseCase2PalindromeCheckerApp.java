cd56797f6fef2d196db1a9a4bb1c74680760ca6f
rg7283-collab <rg7283@srmist.edu.in>
2026-03-18 09:34:56 +0530

    UC2: Hardcoded palindrome

---
+++ src/UseCase2PalindromeCheckerApp.java
@@ -0,0 +1,14 @@
+public class UseCase2PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String word = "madam";
+
+        if(word.equals("madam")){
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is Not a Palindrome");
+        }
+
+    }
+}
